6b1fa1bb32fdc841b59e26af95b2ce0089f78157
Onur Yasar <onur@potamic.uk>
2020-09-25 18:17:02 +0100

    Add option to use phpcbf to auto fix found issues

--- src/main.js
+++ src/main.js
@@ -7,16 +7,22 @@ class IssuesProvider {
         return new Promise(resolve => {
             const issues = []
 
-            const execurePath = nova.config.get('dqh-phpcs.executable-path')
+            const csExecutePath = nova.config.get('dqh-phpcs.cs-executable-path')
+            const autoFix = nova.config.get('dqh-phpcs.use-phpcbf')
+            const cbfExecutePath = nova.config.get('dqh-phpcs.cbf-executable-path')
             const standard = nova.workspace.config.get('dqh-phpcs.standard') ? nova.workspace.config.get('dqh-phpcs.standard') : nova.config.get('dqh-phpcs.standard')
-            const options = {
-                args: [
-                    execurePath,
-                    '--report=json',
-                    `--standard=${standard}`,
-                    document.path
-                ]
-            };
+
+            const args = [
+                csExecutePath,
+                '--report=json',
+                `--standard=${standard}`,
+                document.path
+            ];
+            if(autoFix){
+                args[0] = cbfExecutePath
+            }
+
+            const options = {args};
 
             const process = new Process('/usr/bin/env', options)
 
